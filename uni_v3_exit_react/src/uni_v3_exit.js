469c9fbc5deaf6cc728b01be269f0ac0342f7ae4
lucasmoskun <lucas@lucasmoskun.com>
2021-06-19 23:33:16 +0100

    initial homepage set up

---
+++ uni_v3_exit_react/src/uni_v3_exit.js
@@ -0,0 +1,18 @@
+import titleImg from './img/title.png'
+import './style.css'
+
+import {useEffect } from 'react';
+
+function UNISWAP_V3_EXIT()
+{
+  return(
+    <div className="Home">
+    <header className="Home-header">
+
+      <img src={titleImg} alt="title"/>
+    </header>
+    </div>
+  );
+}
+
+export default UNISWAP_V3_EXIT;
